ad4a172c8343c9ce871f3e7c4f95afc49ae42e95
shizhiguo <shizhiguo@ibeesaas.com>
2018-08-02 19:46:43 +0800

    modify greeRate

--- com/witshare/service/token-distribute-service.js
+++ com/witshare/service/token-distribute-service.js
@@ -76,7 +76,7 @@ const checkUserPayTxValidity = async () => {
                                         updatedItem.userTxStatus = USER_TX_STATUS.CONFIRM_FAIL_FROM_NOT_MATCH;//22
                                     }
                                     //计算应该获得的token数量
-                                    updatedItem.shouldGetAmount = bigDecimal.multiply(updatedItem.actualPayAmount, record.priceRate);
+                                    updatedItem.shouldGetAmount = bigDecimal.round(updatedItem.actualPayAmount * parseFloat(record.priceRate) * (1 + parseFloat(record.freeGiveRate)), 9);
 
                                 } else {
                                     updatedItem.userTxStatus = USER_TX_STATUS.CONFIRM_FAIL_TO_NOT_PLATFORM;//21
@@ -189,7 +189,7 @@ const execTokenDistribute = async (distributionBatchId, project, wallet, params,
                     let userGid = userRecord.userGid;
                     let totalPayCount = userRecord.count;
                     let totalPayAmount = userRecord.totalPayAmount;
-                    let totalShouldGetAmount = bigDecimal.round(bigDecimal.multiply(userRecord.totalShouldGetAmount, 1 + parseFloat(userRecord.freeGiveRate)), tokenDecimal);
+                    let totalShouldGetAmount = bigDecimal.round(userRecord.totalShouldGetAmount, tokenDecimal);
                     logger.info('execTokenDistribute() current userRecord==>userGid=%s; totalPayCount=%s; totalPayAmount=%s; totalShouldGetAmount=%s', userGid, totalPayCount, totalPayAmount, totalShouldGetAmount);
 
                     //获取用户的收币地址
